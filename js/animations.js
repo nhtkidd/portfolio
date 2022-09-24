1cf69bea39e7ba4f0d66236216fef4d6acd1b500
Jesus M <jesusmdz929@gmail.com>
2022-09-24 17:11:40 -0500

    making my portfolio

---
+++ js/animations.js
@@ -0,0 +1,22 @@
+ScrollReveal().reveal(".bienvenida", {
+  duration: 600,
+  distance: "50px",
+  easing: "ease-out",
+  origin: "bottom",
+  reset: true,
+  scale: 1,
+  viewFactor: 0,
+});
+ScrollReveal().reveal(".whoim", {
+  duration: 600,
+  easing: "ease-out",
+  origin: "bottom",
+  reset: true,
+  scale: 1,
+  viewFactor: 0,
+});
+
+
+
+ScrollReveal().reveal(".info", { reset: true, duration: 600 });
+ScrollReveal().reveal(".email", { reset: true, duration: 600 });
